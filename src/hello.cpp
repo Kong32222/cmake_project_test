097d83e086cae6f8b444bc90265a47ff057cd199
kxs <1514817196@qq.com>
2026-02-10 09:57:15 +0800

    version0.0.1

---
+++ src/hello.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+void HelloWorld() 
+{
+    std::cout << "hello world!" << std::endl;
+    std::cout << "hello world!" << std::endl;
+    std::cout << "kong hello world!" << std::endl;
+}
